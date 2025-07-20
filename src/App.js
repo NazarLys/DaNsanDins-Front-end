39be119e7689fbc5db96a34a43e2a57d73ffc618
Tsurimi <nazarlys702@gmail.com>
2025-07-21 01:03:28 +0200

    Cursor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Main from "./pages/Main";
+<<<<<<< Updated upstream
 
 function App() {
   return (
    <Routes>
     <Route path="/" element={<Main />} />
    </Routes>
+=======
+import Cursor from "./components/Cursor";
+
+function App() {
+  return (
+    <>
+   <Routes>
+    <Route path="/" element={<Main />} />
+   </Routes>
+    <Cursor/>
+   </>
+>>>>>>> Stashed changes
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,28 @@
 import { Link, useNavigate } from "react-router-dom";
+<<<<<<< Updated upstream
 import logo from "../assets/logo.png";
+=======
+
+>>>>>>> Stashed changes
 
 const Navbar = () => {
     const navigate = useNavigate();
     return ( 
+<<<<<<< Updated upstream
         <nav className="bg-stone-900 text-white "> 
             <div className=""> 
                 <Link to="/" className=""> 
                 <img src={logo} alt="Logo" className="" />
                 </Link> 
                 </div>   
+=======
+        <nav className="bg-stone-900 text-white p-2 "> 
+            <div className="bg-black-900"> 
+                <Link to="/" className=""> 
+                    <img src="/Logo.svg" alt="Logo" style={{ width: "6rem", height: "auto"}}/>
+                </Link>
+            </div>   
+>>>>>>> Stashed changes
         </nav>
     )
 }
