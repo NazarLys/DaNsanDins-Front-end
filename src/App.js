150382e2b0af75e29bfc9ece2de1ff56b2df67f4
NazarDouble <nazarkoval942@gmail.com>
2025-07-21 11:29:36 +0200

    івів

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Main from "./pages/Main";
-<<<<<<< Updated upstream
-
-function App() {
-  return (
-   <Routes>
-    <Route path="/" element={<Main />} />
-   </Routes>
-=======
 import Cursor from "./components/Cursor";
 
 function App() {
   return (
     <>
-   <Routes>
-    <Route path="/" element={<Main />} />
-   </Routes>
-    <Cursor/>
-   </>
->>>>>>> Stashed changes
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+      <Cursor />
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,16 @@
-import { Link, useNavigate } from "react-router-dom";
-<<<<<<< Updated upstream
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
-=======
-
->>>>>>> Stashed changes
 
 const Navbar = () => {
-    const navigate = useNavigate();
     return ( 
-<<<<<<< Updated upstream
-        <nav className="bg-stone-900 text-white "> 
-            <div className=""> 
-                <Link to="/" className=""> 
-                <img src={logo} alt="Logo" className="" />
-                </Link> 
-                </div>   
-=======
-        <nav className="bg-stone-900 text-white p-2 "> 
-            <div className="bg-black-900"> 
-                <Link to="/" className=""> 
-                    <img src="/Logo.svg" alt="Logo" style={{ width: "6rem", height: "auto"}}/>
+        <nav className="bg-stone-900 text-white p-2"> 
+            <div className="flex items-center">
+                <Link to="/"> 
+                    <img src={logo} alt="Logo" style={{ width: "6rem", height: "auto" }} />
                 </Link>
             </div>   
->>>>>>> Stashed changes
         </nav>
-    )
+    );
 }
+
 export default Navbar;
