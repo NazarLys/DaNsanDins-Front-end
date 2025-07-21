f140811f5da37d4d83f0fb41ef7ad15a8de4242a
NazarDouble <nazarkoval942@gmail.com>
2025-07-21 11:52:12 +0200

    New 1

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,23 @@ import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 
 const Navbar = () => {
-    return ( 
-        <nav style={{ backgroundColor: '#2C2C2C', height: '75px' }} className="text-white p-2">
-            <div className="flex items-center">
-                <Link to="/"> 
-                    <img src={logo} alt="Logo" style={{ width: "60px", height: "60px" }} />
-                </Link>
-            </div>   
-        </nav>
-    );
-}
+  return (
+    <>
+      <nav
+        style={{ backgroundColor: '#2C2C2C', height: '75px' }}
+        className="text-white p-2"
+      >
+        <div className="flex items-center">
+          <Link to="/">
+            <img src={logo} alt="Logo" style={{ width: "60px", height: "60px" }} />
+          </Link>
+        </div>
+      </nav>
+
+      <div style={{ backgroundColor: "#D9D9D9", minHeight: "100vh" }}>
+      </div>
+    </>
+  );
+};
 
 export default Navbar;
